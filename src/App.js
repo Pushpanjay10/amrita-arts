9f628454c3af2f8a4c6179d538420bdd2997966a
adarsh3699 <adarsh3699@gmail.com>
2024-01-10 19:22:38 +0530

    remove useless

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/app.css';
 
 import { handleGetAllImage } from './firebase/home';
